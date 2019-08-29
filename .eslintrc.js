abe264c52a5476180c916c7f3cfa0b21b23d4d10
Sven Münnich <sven.muennich@viison.com>
2019-08-29 19:05:04 +0200

    Use `viison-style-guide` instead of `airbnb-base` ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,6 @@
 module.exports = {
-    'extends': 'airbnb-base',
-    'plugins': [
-        'import',
-        'filenames'
-    ],
-    'rules': {
-        'no-console': 'off',
-        'max-len': ['warn', 120],
-        'no-param-reassign': 'off',
-        'comma-dangle': ['error', {
-            arrays: 'always-multiline',
-            objects: 'always-multiline',
-            imports: 'always-multiline',
-            exports: 'always-multiline',
-            functions: 'never',
-        }],
-        'class-methods-use-this': 'off',
-        'func-names': 'off',
-        'newline-before-return': ['error'],
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
-        'no-use-before-define': ['error', { 'functions': false }],
-        'filenames/match-regex': 'error',
+    extends: './node_modules/viison-style-guide/javascript/modern.js',
+    rules: {
+        'max-len': 'warn',
     },
 };
